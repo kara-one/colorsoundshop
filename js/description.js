0e0b34b1be4080516fd411b1d5fa2fd07e832456
Serhii Karavashkin <master@karaone.info>
2020-08-13 01:13:43 +0300

    fix resize description

--- js/description.js
+++ js/description.js
@@ -1,9 +1,10 @@
-window.onload = function () {
+const addDescriptionButton = function () {
     const productDescription = document.querySelector(
         ".main-product__description-content"
     );
     const productDescriptionHeight = productDescription.clientHeight;
     if (productDescriptionHeight > 200) {
+        productDescription.style.maxHeight = "200px";
         productDescription.classList.add(
             "main-product__description-content_state_collapsed"
         );
@@ -12,7 +13,7 @@ window.onload = function () {
         button.classList.remove("button_type_link_disabled");
 
         button.addEventListener("click", function () {
-            productDescription.style.maxHeight = productDescriptionHeight+"px";
+            productDescription.style.maxHeight = "500vh";
             productDescription.classList.remove(
                 "main-product__description-content_state_collapsed"
             );
@@ -20,3 +21,6 @@ window.onload = function () {
         });
     }
 };
+
+window.addEventListener("load", addDescriptionButton);
+window.addEventListener("resize", addDescriptionButton);
